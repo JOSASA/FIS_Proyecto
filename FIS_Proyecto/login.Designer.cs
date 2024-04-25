d0d9a3db8ee7bf75c0f023d86765ad280f3efad2
Millaness02 <denzelmillanes@gmail.com>
2024-04-24 20:29:07 -0700

    Diseño de Login, El form1 es el menu 2

--- FIS_Proyecto/login.Designer.cs
+++ FIS_Proyecto/login.Designer.cs
@@ -28,67 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
             btnIngresar = new Button();
-            labelUsuario = new Label();
-            labelPassword = new Label();
             txtUsuario = new TextBox();
             txtPassword = new TextBox();
+            panel1 = new Panel();
+            linkLabel1 = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            btnCerrar = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).BeginInit();
             SuspendLayout();
             // 
             // btnIngresar
             // 
-            btnIngresar.Location = new Point(370, 262);
+            btnIngresar.Location = new Point(348, 212);
             btnIngresar.Name = "btnIngresar";
-            btnIngresar.Size = new Size(94, 29);
+            btnIngresar.Size = new Size(338, 40);
             btnIngresar.TabIndex = 0;
-            btnIngresar.Text = "Ingresar";
+            btnIngresar.Text = "ACCEDER";
             btnIngresar.UseVisualStyleBackColor = true;
             btnIngresar.Click += btnIngresar_Click;
             // 
-            // labelUsuario
-            // 
-            labelUsuario.AutoSize = true;
-            labelUsuario.Location = new Point(221, 146);
-            labelUsuario.Name = "labelUsuario";
-            labelUsuario.Size = new Size(59, 20);
-            labelUsuario.TabIndex = 1;
-            labelUsuario.Text = "Usuario";
-            // 
-            // labelPassword
-            // 
-            labelPassword.AutoSize = true;
-            labelPassword.Location = new Point(197, 196);
-            labelPassword.Name = "labelPassword";
-            labelPassword.Size = new Size(83, 20);
-            labelPassword.TabIndex = 2;
-            labelPassword.Text = "Contraseña";
-            // 
             // txtUsuario
             // 
-            txtUsuario.Location = new Point(351, 146);
+            txtUsuario.BackColor = Color.WhiteSmoke;
+            txtUsuario.BorderStyle = BorderStyle.None;
+            txtUsuario.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtUsuario.Location = new Point(336, 98);
             txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(125, 27);
+            txtUsuario.Size = new Size(380, 22);
             txtUsuario.TabIndex = 3;
+            txtUsuario.Text = "USUARIO";
+            txtUsuario.TextChanged += txtUsuario_TextChanged;
+            txtUsuario.Enter += txtUsuario_Enter;
+            txtUsuario.Leave += txtUsuario_Leave;
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(351, 196);
+            txtPassword.BackColor = Color.WhiteSmoke;
+            txtPassword.BorderStyle = BorderStyle.None;
+            txtPassword.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPassword.Location = new Point(336, 154);
             txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(125, 27);
+            txtPassword.Size = new Size(380, 22);
             txtPassword.TabIndex = 4;
+            txtPassword.Text = "CONTRASEÑA";
+            txtPassword.Enter += txtPassword_Enter;
+            txtPassword.Leave += txtPassword_Leave;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 122, 204);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(250, 330);
+            panel1.TabIndex = 5;
+            panel1.MouseDown += panel1_MouseDown;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(410, 267);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(208, 17);
+            linkLabel1.TabIndex = 6;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "¿HE OLVIDADO MI CONTRASEÑA?";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.DimGray;
+            pictureBox1.Enabled = false;
+            pictureBox1.Location = new Point(336, 117);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(380, 1);
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.DimGray;
+            pictureBox2.Enabled = false;
+            pictureBox2.Location = new Point(336, 173);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(380, 1);
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Image = (Image)resources.GetObject("btnCerrar.Image");
+            btnCerrar.Location = new Point(757, 2);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(20, 20);
+            btnCerrar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnCerrar.TabIndex = 9;
+            btnCerrar.TabStop = false;
+            btnCerrar.Click += btnCerrar_Click;
             // 
             // login
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.WhiteSmoke;
+            ClientSize = new Size(780, 330);
+            Controls.Add(btnCerrar);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(linkLabel1);
+            Controls.Add(panel1);
             Controls.Add(txtPassword);
             Controls.Add(txtUsuario);
-            Controls.Add(labelPassword);
-            Controls.Add(labelUsuario);
             Controls.Add(btnIngresar);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "login";
             Text = "Login";
+            MouseDown += login_MouseDown;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -96,9 +158,12 @@
         #endregion
 
         private Button btnIngresar;
-        private Label labelUsuario;
-        private Label labelPassword;
         private TextBox txtUsuario;
         private TextBox txtPassword;
+        private Panel panel1;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox btnCerrar;
     }
 }
--- FIS_Proyecto/login.cs
+++ FIS_Proyecto/login.cs
@@ -3,13 +3,18 @@
 namespace FIS_Proyecto
 {
     using BussinesRules;
+    using System.Runtime.InteropServices;
+
     public partial class login : Form
     {
         public login()
         {
             InitializeComponent();
         }
-
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private static extern void SendMessages(System.IntPtr hwnd, int wnsg, int wparam, int lparam);
 
         private void btnIngresar_Click(object sender, EventArgs e)
         {
@@ -41,5 +46,64 @@ namespace FIS_Proyecto
             }
         }
 
+        private void txtUsuario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "USUARIO")
+            {
+                txtUsuario.Text = "";
+                txtUsuario.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "USUARIO";
+                txtUsuario.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "CONTRASEÑA")
+            {
+                txtPassword.Text = "";
+                txtPassword.ForeColor = Color.Black;
+                txtPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "")
+            {
+                txtPassword.Text = "CONTRASEÑA";
+                txtPassword.ForeColor = Color.Black;
+                txtPassword.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void login_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessages(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessages(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }

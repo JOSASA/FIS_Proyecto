4df474b6f764cadc12e3c67135570191bd28fcfb
Mercur06 <alfredonavaso12@gmail.com>
2024-05-07 13:37:05 -0700

    Termine por lo pronto el form compras y clientes ademas de centrar el login al inicio y cree la tabla compras y clientes de la base de datos.

--- PROYECTO/Forms/frmCompras.Designer.cs
+++ PROYECTO/Forms/frmCompras.Designer.cs
@@ -28,19 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvCompra = new System.Windows.Forms.DataGridView();
+            this.btnAgregarCarro = new System.Windows.Forms.Button();
+            this.lstCarro = new System.Windows.Forms.ListBox();
+            this.cmbCompras = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompra)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvCompra
+            // 
+            this.dgvCompra.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCompra.Location = new System.Drawing.Point(436, 123);
+            this.dgvCompra.Name = "dgvCompra";
+            this.dgvCompra.Size = new System.Drawing.Size(340, 150);
+            this.dgvCompra.TabIndex = 0;
+            // 
+            // btnAgregarCarro
+            // 
+            this.btnAgregarCarro.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregarCarro.Location = new System.Drawing.Point(298, 123);
+            this.btnAgregarCarro.Name = "btnAgregarCarro";
+            this.btnAgregarCarro.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnAgregarCarro.Size = new System.Drawing.Size(118, 23);
+            this.btnAgregarCarro.TabIndex = 1;
+            this.btnAgregarCarro.Text = "Agregar al carrito";
+            this.btnAgregarCarro.UseVisualStyleBackColor = true;
+            this.btnAgregarCarro.Click += new System.EventHandler(this.btnAgregarCarro_Click);
+            // 
+            // lstCarro
+            // 
+            this.lstCarro.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstCarro.FormattingEnabled = true;
+            this.lstCarro.Location = new System.Drawing.Point(298, 178);
+            this.lstCarro.Name = "lstCarro";
+            this.lstCarro.Size = new System.Drawing.Size(118, 95);
+            this.lstCarro.TabIndex = 3;
+            // 
+            // cmbCompras
+            // 
+            this.cmbCompras.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCompras.FormattingEnabled = true;
+            this.cmbCompras.Location = new System.Drawing.Point(155, 123);
+            this.cmbCompras.Name = "cmbCompras";
+            this.cmbCompras.Size = new System.Drawing.Size(121, 21);
+            this.cmbCompras.TabIndex = 4;
             // 
             // frmCompras
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbCompras);
+            this.Controls.Add(this.lstCarro);
+            this.Controls.Add(this.btnAgregarCarro);
+            this.Controls.Add(this.dgvCompra);
             this.Name = "frmCompras";
             this.Text = "Compras";
+            this.Load += new System.EventHandler(this.frmCompras_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompra)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvCompra;
+        private System.Windows.Forms.Button btnAgregarCarro;
+        private System.Windows.Forms.ListBox lstCarro;
+        private System.Windows.Forms.ComboBox cmbCompras;
     }
 }
--- PROYECTO/Forms/frmCompras.cs
+++ PROYECTO/Forms/frmCompras.cs
@@ -7,14 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Data;
 
 namespace PROYECTO.Forms
 {
     public partial class frmCompras : Form
     {
+        private ConexionSQL conexionSQL = new ConexionSQL();
+        private List<string> carrito = new List<string>();
         public frmCompras()
         {
             InitializeComponent();
+        }
+
+        private void frmCompras_Load(object sender, EventArgs e)
+        {
+            dgvCompra.DataSource = conexionSQL.ObtenerCompra();
+            CargarDatosEnComboBox();
+        }
+        private void CargarDatosEnComboBox()
+        {
+            try
+            {
+                DataTable dtCompras = conexionSQL.ObtenerCompra();
+                if (dtCompras.Rows.Count > 0)
+                {
+                    cmbCompras.Items.Clear();
+                    foreach (DataRow row in dtCompras.Rows)
+                    {
+                        string nombreProducto = row["Producto"].ToString();
+                        cmbCompras.Items.Add(nombreProducto);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No se encontraron productos.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los datos: " + ex.Message);
+            }
+
+        }
+
+        private void btnAgregarCarro_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nombreProducto = cmbCompras.SelectedItem.ToString();
+                carrito.Add(nombreProducto);
+                MostrarProductosEnCarrito();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al agregar al carrito: " + ex.Message);
+            }
+        }
+        private void MostrarProductosEnCarrito()
+        {
+            lstCarro.Items.Clear();
+            foreach (string producto in carrito)
+            {
+                lstCarro.Items.Add(producto);
+            }
         }
     }
 }
